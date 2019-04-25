f71ebdb32b3a9b92c3f61ffca825b66a39633ca2
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2019-04-25 14:01:39 +0530

    snapshot

--- src/main/java/io/mosip/ivv/core/structures/Scenario.java
+++ src/main/java/io/mosip/ivv/core/structures/Scenario.java
@@ -20,52 +20,6 @@ public class Scenario {
 
     @Getter
     @Setter
-    public static class Step
-    {
-        private String name = ""; // needs to be passed
-        private String variant = "DEFAULT"; // default
-        private ArrayList<Assert> asserts;
-        private ArrayList<Error> errors;
-        private int AssertionPolicy = 0; // default
-        private boolean FailExpected = false; //default
-        private ArrayList<String> parameters;
-        private ArrayList<Integer> index;
-
-        public class Error{
-            public ErrorPolicy type;
-            public ArrayList<String> parameters;
-            public Error(ErrorPolicy t, ArrayList<String> p) {
-                this.type = t;
-                this.parameters = p;
-            }
-        }
-
-        public class Assert{
-            public AssertionPolicy type;
-            public ArrayList<String> parameters;
-            public Assert(AssertionPolicy t, ArrayList<String> p) {
-                this.type = t;
-                this.parameters = p;
-            }
-        }
-
-        public Step(){
-
-        }
-
-        public Step(String name, String variant, ArrayList<Assert> asserts, ArrayList<Error> errors, ArrayList<String> parameters, ArrayList<Integer> index)
-        {
-            this.name = name;
-            this.variant = variant;
-            this.asserts = asserts;
-            this.errors = errors;
-            this.parameters = parameters;
-            this.index = index;
-        }
-    }
-
-    @Getter
-    @Setter
     public static class Data
     {
         private String personaClass,tag,groupName;
---
+++ src/main/java/io/mosip/ivv/core/structures/Step.java
@@ -0,0 +1,54 @@
+package main.java.io.mosip.ivv.core.structures;
+
+import lombok.Getter;
+import lombok.Setter;
+import main.java.io.mosip.ivv.core.policies.AssertionPolicy;
+import main.java.io.mosip.ivv.core.policies.ErrorPolicy;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class Step
+{
+    private String name = ""; // needs to be passed
+    private String variant = "DEFAULT"; // default
+    private ArrayList<Assert> asserts;
+    private ArrayList<Error> errors;
+    private int AssertionPolicy = 0; // default
+    private boolean FailExpected = false; //default
+    private ArrayList<String> parameters;
+    private ArrayList<Integer> index;
+
+    public class Error{
+        public ErrorPolicy type;
+        public ArrayList<String> parameters;
+        public Error(ErrorPolicy t, ArrayList<String> p) {
+            this.type = t;
+            this.parameters = p;
+        }
+    }
+
+    public class Assert{
+        public main.java.io.mosip.ivv.core.policies.AssertionPolicy type;
+        public ArrayList<String> parameters;
+        public Assert(AssertionPolicy t, ArrayList<String> p) {
+            this.type = t;
+            this.parameters = p;
+        }
+    }
+
+    public Step(){
+
+    }
+
+    public Step(String name, String variant, ArrayList<Assert> asserts, ArrayList<Error> errors, ArrayList<String> parameters, ArrayList<Integer> index)
+    {
+        this.name = name;
+        this.variant = variant;
+        this.asserts = asserts;
+        this.errors = errors;
+        this.parameters = parameters;
+        this.index = index;
+    }
+}
