1812cf0d30d2d61bbc4022db7c4f281308eb9559
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2019-04-25 14:36:00 +0530

    snapshot

--- src/main/java/io/mosip/ivv/core/structures/Scenario.java
+++ src/main/java/io/mosip/ivv/core/structures/Scenario.java
@@ -18,8 +18,50 @@ public class Scenario {
     private ArrayList<String> tags = new ArrayList();
     private String personaClass, groupName;
 
-    @Getter
-    @Setter
+    public static class Step
+    {
+        private String name = ""; // needs to be passed
+        private String variant = "DEFAULT"; // default
+        private ArrayList<Assert> asserts;
+        private ArrayList<Error> errors;
+        private int AssertionPolicy = 0; // default
+        private boolean FailExpected = false; //default
+        private ArrayList<String> parameters;
+        private ArrayList<Integer> index;
+
+        public static class Error{
+            public ErrorPolicy type;
+            public ArrayList<String> parameters;
+            public Error(ErrorPolicy t, ArrayList<String> p) {
+                this.type = t;
+                this.parameters = p;
+            }
+        }
+
+        public static class Assert{
+            public main.java.io.mosip.ivv.core.policies.AssertionPolicy type;
+            public ArrayList<String> parameters;
+            public Assert(AssertionPolicy t, ArrayList<String> p) {
+                this.type = t;
+                this.parameters = p;
+            }
+        }
+
+        public Step(){
+
+        }
+
+        public Step(String name, String variant, ArrayList<Assert> asserts, ArrayList<Error> errors, ArrayList<String> parameters, ArrayList<Integer> index)
+        {
+            this.name = name;
+            this.variant = variant;
+            this.asserts = asserts;
+            this.errors = errors;
+            this.parameters = parameters;
+            this.index = index;
+        }
+    }
+
     public static class Data
     {
         private String personaClass,tag,groupName;
@@ -35,4 +77,5 @@ public class Scenario {
     private Data data = null;
     private boolean continueOnFailure = false; // default
     private boolean isFailureExpected = false; // default
+
 }
