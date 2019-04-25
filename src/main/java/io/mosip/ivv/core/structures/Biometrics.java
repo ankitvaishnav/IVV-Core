9c7685a5bf0b64f965b8c5fb6c288842ae38f8ec
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2019-04-25 15:06:48 +0530

    snapshot

--- src/main/java/io/mosip/ivv/core/structures/Biometrics.java
+++ src/main/java/io/mosip/ivv/core/structures/Biometrics.java
@@ -1,13 +1,19 @@
 package main.java.io.mosip.ivv.core.structures;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Biometrics {
 
     public enum BIOMETRIC_TYPE {FACE, IRIS, FINGER};
     public enum BIOMETRIC_CAPTURE {LEFT_THUMB, RIGHT_THUMB, TWO_THUMBS, LEFT_SLAP, RIGHT_SLAP, TEN_FINGERS, LEFT_EYE, RIGHT_EYE, STILL_PHOTO, LIVE_PHOTO};
 
-    public BIOMETRIC_TYPE type = BIOMETRIC_TYPE.FINGER;
-    public BIOMETRIC_CAPTURE capture = BIOMETRIC_CAPTURE.TWO_THUMBS;
-    public byte[] rawImage = null;
-    public String base64EncodedImage= "";
+    private BIOMETRIC_TYPE type = BIOMETRIC_TYPE.FINGER;
+    private BIOMETRIC_CAPTURE capture = BIOMETRIC_CAPTURE.TWO_THUMBS;
+    private Object object = null;
+    private byte[] rawImage = null;
+    private String base64EncodedImage= "";
 
 }
